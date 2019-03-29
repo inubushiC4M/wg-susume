46f4da68404883c0d53b4add7ea06293fe6c1abc
inubushiC4M <y.inubushi@c4m.co.jp>
2019-03-29 16:21:11 +0900

    【下層ページ作成】STYLE追加（PC）

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -2,3 +2,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var scroll = new SmoothScroll('a[href*="#"]');
 });
+
+//modaal
+$('.modal').modaal();
+$('.my-link').modaal({
+    hide_close: true,
+    close_text: 'とじる'
+});
